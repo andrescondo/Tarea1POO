373995ca13a2561c7ff1716869c919c52a806045
andrescondo <1dkevinandres.b@gmail.com>
2021-12-27 19:54:20 -0500

    adiccion de carpeta para el programa de nombre completo

---
+++ NombreCompleto/src/nombrecompleto/NombreCompleto.java
@@ -0,0 +1,13 @@
+package nombrecompleto;
+/**
+ * Mostrar el nombre completo en pantalla
+ * 
+ * @author Kevin Andres Condo Chaguay
+ * @version POO - 2021
+ * @since Desde 27-12-2021, 16:45 horas
+ */
+public class NombreCompleto { 
+    public static void main(String[] args) { //inicio de la metodo main
+        System.out.println("Kevin Andres Condo Chaguay");//impresión en pantalla del nombre
+    }//fin de la metodo main
+}
